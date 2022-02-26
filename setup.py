8262974781f3ac019909e8a9f869da5a0cdabe34
André Carvalho <afecarvalho@gmail.com>
2022-02-26 18:44:27 +0000

    Add repo c to deps

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="repo_b",
     version="0.0.1",
-    install_requires=["pymongo"],
+    install_requires=["pymongo", "repo_c"],
     packages=find_packages("."),
 )
