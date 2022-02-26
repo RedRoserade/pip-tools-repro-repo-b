68e7bfa5be2151ab42fc1e890195fa6137bd085c
André Carvalho <afecarvalho@gmail.com>
2022-02-26 19:07:20 +0000

    Temp: remove dep on C

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="repo_b",
     version="0.0.1",
-    install_requires=["pymongo", "repo_c"],
+    # install_requires=["pymongo", "repo_c"],
+    install_requires=["pymongo"],
     packages=find_packages("."),
 )
