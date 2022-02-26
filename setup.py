1706abccd7685d5a0824c473ecaed931a506333d
André Carvalho <afecarvalho@gmail.com>
2022-02-26 19:09:51 +0000

    Experiment: use PEP 440 URL

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="repo_b",
     version="0.0.1",
+    install_requires=[
+        "pymongo", 
+        "repo_c @ https://github.com/RedRoserade/pip-tools-repro-repo-c.git",
+    ],
+    # install_requires=["pymongo"],
     # install_requires=["pymongo", "repo_c"],
-    install_requires=["pymongo"],
     packages=find_packages("."),
 )
